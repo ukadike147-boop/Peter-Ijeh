50db21b8f039e04ac725cae58c5f14507d76750d
ukadike147-boop <ukadike147@gmail.com>
2026-08-13 14:01:19 -0500

    Save Hot Collections progress

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
